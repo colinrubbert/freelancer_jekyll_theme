22e68b04d9108d000fbd0122198b2882b689c5e6
Colin Rubbert <colinrubbert@gmail.com>
2017-02-27 10:52:27 -0600

    initial commit, build out theme

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+// Main JavaScript File
+
+// Toggle Navigation Menu
+
+var toggleButton = document.getElementById("navigation-menu-toggle").classList;
+var menu = document.getElementById("navigation-links-wrapper").classList;
+
+function toggleMenu() {
+  menu.toggle("open");
+}
